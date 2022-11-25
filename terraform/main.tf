f95d8f362cb78f277945cfdd0dcb0e3ddff2c27d
jlk <jkendall1975@gmail.com>
2022-11-25 11:26:41 -0500

    tf update

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ provider "linode" {
   token = var.linode_token
 }
 
-/*
+
 resource "linode_firewall" "my_firewall" {
   label = "my_firewall"
 
@@ -22,7 +22,7 @@ resource "linode_firewall" "my_firewall" {
     ipv4     = ["0.0.0.0/0"]
     ipv6     = ["::/0"]
   }
-
+/*
   inbound {
     label    = "allow-http"
     action   = "ACCEPT"
@@ -49,35 +49,34 @@ resource "linode_firewall" "my_firewall" {
     ipv4     = ["0.0.0.0/0"]
     ipv6     = ["::/0"]
   }
-
+*/
   inbound_policy = "DROP"
 
   outbound_policy = "ACCEPT"
 
-  linodes = [linode_instance.terraform-control.id]
+  linodes = [linode_instance.terraform-gpserver.id]
 }
 
 
-resource "linode_stackscript" "terraform-control" {
+resource "linode_stackscript" "terraform-gpserver" {
   label = "foo"
   description = "Sets hostname"
   script = <<EOF
 #!/bin/bash
-hostnamectl set-hostname controller01
+hostnamectl set-hostname gpserver
 EOF
   images = ["linode/debian10"]
   rev_note = "initial version"
 }
 
-resource "linode_instance" "terraform-control" {
+resource "linode_instance" "terraform-gpserver" {
         image = "linode/debian10"
-        label = "Terraform-Control"
+        label = "Terraform-GP"
         group = "Terraform"
         region = "us-east"
         type = "g6-nanode-1"
         authorized_keys = [var.authorized_keys]
         root_pass = var.root_pass
 
-        stackscript_id = linode_stackscript.terraform-control.id
+        stackscript_id = linode_stackscript.terraform-gpserver.id
 }
-*/
